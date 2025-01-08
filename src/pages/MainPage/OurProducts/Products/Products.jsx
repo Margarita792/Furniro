94fe691ded28f623da9110236a7a2be675a1f682
Margarita792 <ritagru92@gmail.com>
2025-01-08 14:58:46 +0200

    fix: fixed components

--- src/pages/MainPage/OurProducts/Products/Products.jsx
+++ src/pages/MainPage/OurProducts/Products/Products.jsx
@@ -3,7 +3,7 @@ import ProductOrdinary from './ProductOrdinary/ProductOrdinary'
 import ProductNew from './ProductNew/ProductNew'
 import style from '../Products/Products.module.css'
 import { Link } from 'react-router-dom'
-function Products({ products}) {
+function Products({ products }) {
     return (<>
 
         <h1 className={style.textOurProducts}>Our Products</h1>
@@ -11,7 +11,49 @@ function Products({ products}) {
             <div className={style.wrapperProducts}>
 
 
-                {products.slice(0, 8).map((item, index) => {
+                {products.slice(0, 4).map((item, index) => {
+                    if (item.state === "discount") {
+                        return (
+                            <Link key={index} className={style.link} to={`/shop/product/${index}`}>
+                                <ProductDiscount
+                                    discount={item.discount}
+                                    img={item.img}
+                                    title={item.title}
+                                    description={item.description}
+                                    price={item.price}
+                                    sale={item.sale} />
+                            </Link>
+                        )
+                    }
+                    if (item.state === "default") {
+                        return (
+                            <Link key={index} className={style.link} to={`/shop/product/${index}`}>
+                                <ProductOrdinary
+                                    img={item.img}
+                                    title={item.title}
+                                    description={item.description}
+                                    price={item.price} />
+                            </Link>
+                        )
+                    }
+                    if (item.state === "new") {
+                        return (
+                            <Link key={index} className={style.link} to={`/shop/product/${index}`}>
+                                <ProductNew
+                                    img={item.img}
+                                    title={item.title}
+                                    description={item.description}
+                                    price={item.price} />
+                            </Link>
+                        )
+                    }
+                    return null;
+                })}
+            </div>
+            <div className={style.wrapperProducts}>
+
+
+                {products.slice(4, 8).map((item, index) => {
                     if (item.state === "discount") {
                         return (
                             <Link key={index} className={style.link} to={`/shop/product/${index}`}>
